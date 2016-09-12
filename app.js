f64a996f721076e92d271cda34aec97414b1a8c8
Michael Liendo <model3volution@gmail.com>
2016-09-12 12:02:42 -0500

    scraped with static search complete

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
 
-request('http://imgur.com/t/javascript', function(err, res, body) {
+var images = [];
+request('http://imgur.com/t/javascript',function(err, res, body) {
   if(!err && res.statusCode === 200) {
-    console.log(body);
+    var $ = cheerio.load(body);
+    $('img', '.cards').each(function() {
+      var img = $(this).attr('src');
+      img = 'http:' + img;
+      images.push(img);
+    });
   }
+  images.forEach(function(val,i,arr) {
+    request(val).pipe(fs.createWriteStream('./imgs/img' + i +'.jpg'));
+  });
 });
