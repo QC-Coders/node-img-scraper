7f3eb6f4f1f38353e774ba353e6e87be6a9f41ff
Michael Liendo <model3volution@gmail.com>
2016-09-12 12:20:36 -0500

    successfully added yargs with search option

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
+var argv = require('yargs').argv;
+
+var searchTopic = argv.search;
 
 var images = [];
-request('http://imgur.com/t/javascript',function(err, res, body) {
-  if(!err && res.statusCode === 200) {
-    var $ = cheerio.load(body);
-    $('img', '.cards').each(function() {
-      var img = $(this).attr('src');
-      img = 'http:' + img;
-      images.push(img);
+
+  request('http://imgur.com/t/' + searchTopic,function(err, res, body) {
+    if(!err && res.statusCode === 200) {
+      var $ = cheerio.load(body);
+      $('img', '.cards').each(function() {
+        var img = $(this).attr('src');
+        img = 'http:' + img;
+        images.push(img);
+      });
+    }
+    images.forEach(function(val,i,arr) {
+      request(val).pipe(fs.createWriteStream('./imgs/img' + i +'.jpg'));
     });
-  }
-  images.forEach(function(val,i,arr) {
-    request(val).pipe(fs.createWriteStream('./imgs/img' + i +'.jpg'));
   });
-});
